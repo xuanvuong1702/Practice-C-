41f7d9723f1ef2dae8d900745b8971c7dfc62880
Phung Xuan Vuong <phungxuanvuong97@gmail.com>
2024-09-29 11:46:19 +0700

    add source code for build with cmake vs code

--- main.cpp
+++ main.cpp
@@ -1,29 +1,6 @@
-#include "leetcode/MinimumWindow.h"
-#include "leetcode/LinkedListCircle.h"
-#include "leetcode/DuplicateNumber.h"
-#include "leetcode/SearchNumber.h"
-#include "leetcode/LargesRectangle.h"
-#include "leetcode/TopKElement.h"
-#include "leetcode/TopFrequent.h"
-#include "leetcode/KPairSmallest.h"
-#include "leetcode/MergeInterval.h"
-#include "leetcode/BinaryTree.h"
-#include "AIDD/Mock1.h"
-#include "AIDD/Round2Mock.h"
-#include "ProgrammerKr/SortString.h"
-#include "AIDD/ChoseExecise.h"
-#include "codeforce/DpFibonacci.h"
-#include "leetcode/ClimbingStair.h"
-#include "leetcode/HouseRobber.h"
-#include "leetcode-withAI/CoinChange.h"
-#include "leetcode-withAI/ChoosingAProblem.hpp"
-#include "test/ClassNS.hpp"
-#include "leetcode-withAI/LargestRectangle.hpp"
-
-int main(){
-
-    //Round2Mock1::Solution();
-    Round2Mock::Solution7();
+int main()
+{
+    
     return 0;
 }
 
