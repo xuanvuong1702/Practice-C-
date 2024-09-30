99a0ec6a71bf6313c32ec66d52bbd2d69cf955a5
phungvuong97 <trachanhvn.tk@gmail.com>
2024-09-30 09:37:01 +0700

    chore: Update CMakeLists.txt to include chapter2_process and apps directories

--- system-programming/apps/main.c
+++ system-programming/apps/main.c
@@ -1,11 +1,13 @@
 #include "string_struct.h"
+#include "forking.h"
 #include <stdio.h>
 
 
 int main()
 {
     //chapter 1 part 4: string and struct
-    RunMemoryManagement();
+    //RunMemoryManagement();
+    SimpleFork();
 
     return 0;
 }
